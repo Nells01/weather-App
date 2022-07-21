5c765ef4f2090aae59bf379fe356d3f550d4d55e
Nells01 <98474815+Nells01@users.noreply.github.com>
2022-07-21 09:35:01 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let weather = {
+    apiKey: "3737927ebeb682d1edae2ee2b6eb7107",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&units=imperial&appid=" 
+            + this.apiKey
+        )
+            .then ((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + "Mph";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage= "url('https://source.unsplash.com/1600x900/?" + name + "')"
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+};
+
+document.querySelector(".search button").addEventListener("click", () => {
+        weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if(event.key == "Enter") {
+        weather.search();
+    }
+});
+
+weather.fetchWeather("Raleigh");
+
